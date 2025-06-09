5f4da5a559edeebedef0a01b28d1bb6960d7d842
tabris913 <tabris913@gmail.com>
2025-06-09 17:36:15 +0900

    modified archive script

--- archive.sh
+++ archive.sh
@@ -2,7 +2,12 @@
 
 FILENAME=nacht_server_addon_v${npm_package_version}
 
-zip -r releases/${FILENAME}.zip dist/* README.md
+rm -f releases/${FILENAME}.zip
+
+cd dist
+zip -r ../releases/${FILENAME}.zip *
+cd -
+zip releases/${FILENAME}.zip -u README.md
 cp releases/${FILENAME}.zip releases/${FILENAME}.mcaddon
 
 rm -rf ../development_behavior_packs/nacht_server_BP ../development_resource_packs/nacht_server_RP
