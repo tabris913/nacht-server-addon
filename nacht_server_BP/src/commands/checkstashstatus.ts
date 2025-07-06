65620792041ea12d2a87fb0589b2f8e467c9d5da
tabris913 <tabris913@gmail.com>
2025-07-06 14:34:02 +0900

    #36 modified to job

--- nacht_server_BP/src/commands/checkstashstatus.ts
+++ nacht_server_BP/src/commands/checkstashstatus.ts
@@ -26,38 +26,43 @@ const checkStashStatusCommand: CustomCommand = {
 const commandProcess = (origin: CustomCommandOrigin, targets: Array<Player>): CustomCommandResult => {
   const number_of_stashes = (world.getDynamicProperty(VAR_STASH) as number | undefined) || 0;
 
-  targets.forEach((target) => {
-    let counter = 0;
-    const messages: Array<string> = [];
-    Array.from({ length: number_of_stashes }).forEach((_, index) => {
-      let msg = `${(index + 1).toString().padStart(2, '0')}: `;
+  system.runJob(
+    (function* () {
+      for (const target of targets) {
+        let counter = 0;
+        const messages: Array<string> = [];
+        Array.from({ length: number_of_stashes }).forEach((_, index) => {
+          let msg = `${(index + 1).toString().padStart(2, '0')}: `;
 
-      if (target.hasTag(`stash${index + 1}`)) {
-        counter++;
-        msg += '発見済';
-      } else {
-        msg += '未発見';
+          if (target.hasTag(`stash${index + 1}`)) {
+            counter++;
+            msg += '発見済';
+          } else {
+            msg += '未発見';
+          }
+          messages.push(msg);
+        });
+
+        const form = new ActionFormData();
+        form.title('隠しスタッシュ発見状況');
+        form.body(`現在隠しスタッシュは全部で ${number_of_stashes} 個存在します。`);
+        form.body(`発見状況 [${counter} / ${number_of_stashes}]\n${messages.join('\n')}`);
+        form.button('閉じる');
+
+        // 完了を待つ必要なし
+        yield void form
+          .show(target as any)
+          .then((response) => {
+            if (response.canceled) {
+              Logger.log(`[${target.nameTag}] canceled: ${response.cancelationReason}`);
+
+              return;
+            }
+          })
+          .catch(() => null);
       }
-      messages.push(msg);
-    });
-
-    const form = new ActionFormData();
-    form.title('隠しスタッシュ発見状況');
-    form.body(`現在隠しスタッシュは全部で ${number_of_stashes} 個存在します。`);
-    form.body(`発見状況 [${counter} / ${number_of_stashes}]\n${messages.join('\n')}`);
-    form.button('閉じる');
-
-    form
-      .show(target as any)
-      .then((response) => {
-        if (response.canceled) {
-          Logger.log(`[${target.nameTag}] canceled: ${response.cancelationReason}`);
-
-          return;
-        }
-      })
-      .catch(() => null);
-  });
+    })()
+  );
 
   return { status: CustomCommandStatus.Success };
 };
