5b77ae2e2023908619d2236a69fad209baa6c197
tabris913 <tabris913@gmail.com>
2025-07-16 01:27:31 +0900

    added nocturium

--- add_ore.sh
+++ add_ore.sh
@@ -66,7 +66,14 @@ for part in _boots _chestplate _helmet _leggings; do
 done
 mkdir -p $BP/items/tools/${METAL_NAME}
 for part in _sword _axe _pickaxe _shovel _hoe; do
-  cat templates/items$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/items/tools/${METAL_NAME}/${METAL_NAME}$part.json
+  if [ "$2" = "1" ]; then
+    if [ $part != "_sword" ]; then
+      continue
+    fi
+    cat templates/items$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/items/tools/${METAL_NAME}$part.json
+  else
+    cat templates/items$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/items/tools/${METAL_NAME}/${METAL_NAME}$part.json
+  fi
 done
 
 mkdir -p $BP/recipes/armor/${METAL_NAME}
@@ -75,7 +82,14 @@ for part in _boots _chestplate _helmet _leggings; do
 done
 mkdir -p $BP/recipes/tools/${METAL_NAME}
 for part in _sword _axe _pickaxe _shovel _hoe; do
-  cat templates/recipes$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/recipes/tools/${METAL_NAME}/${METAL_NAME}$part.json
+  if [ "$2" = "1" ]; then
+    if [ $part != "_sword" ]; then
+      continue
+    fi
+    cat templates/recipes$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/recipes/tools/${METAL_NAME}$part.json
+  else
+    cat templates/recipes$part.json | sed s/\$ITEM_NAME/${METAL_NAME}/g > $BP/recipes/tools/${METAL_NAME}/${METAL_NAME}$part.json
+  fi
 done
 
 mkdir -p $RP/attachables/${METAL_NAME}
@@ -90,6 +104,9 @@ for part in _boots _chestplate _helmet _leggings; do
 done
 mkdir -p $RP/textures/items/tools/${METAL_NAME}
 for part in _sword _axe _pickaxe _shovel _hoe; do
+  if [ "$2" = "1" -a $part != "_sword" ]; then
+    continue
+  fi
   cp .vscode/bedrock-samples-1.21.90.3/resource_pack/textures/items/iron$part.png $RP/textures/items/tools/${METAL_NAME}/${METAL_NAME}$part.png
 done
 
@@ -98,60 +115,60 @@ cp .vscode/bedrock-samples-1.21.90.3/resource_pack/textures/models/armor/iron_1.
 
 echo Don\'t forget to add to below files
 echo $BP/item_catalog/crafting_item_catalog.json
-echo lang.txt
+# echo lang.txt
 
-echo "// $RP/texts/ja_JP.lang" > lang.txt
-echo "## ${METAL_NAME}" >> lang.txt
-echo "item.nacht:raw_${METAL_NAME}.name=${2}の原石	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_ingot.name=${2}のインゴット	##" >> lang.txt
-echo "tile.nacht:${METAL_NAME}_ore.name=${2}鉱石	##" >> lang.txt
-echo "tile.nacht:deepslate_${METAL_NAME}_ore.name=深層岩${2}鉱石	##" >> lang.txt
-echo "tile.nacht:${METAL_NAME}_block.name=${2}ブロック	##" >> lang.txt
-echo "tile.nacht:raw_${METAL_NAME}_block.name=${2}の原石ブロック	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_boots.name=${2}のブーツ	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_chestplate.name=${2}のチェストプレート	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_helmet.name=${2}のヘルメット	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_leggings.name=${2}のレギンス	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_sword.name=${2}の剣	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_axe.name=${2}の斧	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_pickaxe.name=${2}のピッケル	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_shovel.name=${2}のシャベル	##" >> lang.txt
-echo "item.nacht:${METAL_NAME}_hoe.name=${2}のクワ	##" >> lang.txt
-echo >> lang.txt
+# echo "// $RP/texts/ja_JP.lang" > lang.txt
+# echo "## ${METAL_NAME}" >> lang.txt
+# echo "item.nacht:raw_${METAL_NAME}.name=${2}の原石	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_ingot.name=${2}のインゴット	##" >> lang.txt
+# echo "tile.nacht:${METAL_NAME}_ore.name=${2}鉱石	##" >> lang.txt
+# echo "tile.nacht:deepslate_${METAL_NAME}_ore.name=深層岩${2}鉱石	##" >> lang.txt
+# echo "tile.nacht:${METAL_NAME}_block.name=${2}ブロック	##" >> lang.txt
+# echo "tile.nacht:raw_${METAL_NAME}_block.name=${2}の原石ブロック	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_boots.name=${2}のブーツ	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_chestplate.name=${2}のチェストプレート	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_helmet.name=${2}のヘルメット	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_leggings.name=${2}のレギンス	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_sword.name=${2}の剣	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_axe.name=${2}の斧	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_pickaxe.name=${2}のピッケル	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_shovel.name=${2}のシャベル	##" >> lang.txt
+# echo "item.nacht:${METAL_NAME}_hoe.name=${2}のクワ	##" >> lang.txt
+# echo >> lang.txt
 
-echo "// $RP/textures/item_texture.json" >> lang.txt
-echo ",\"nacht:raw_${METAL_NAME}\":{\"textures\":\"textures/items/metal/${METAL_NAME}/raw_${METAL_NAME}\"}" >> lang.txt
-for part in ingot sword axe pickaxe shovel hoe boots chestplate helmet leggings; do
-  echo ",\"nacht:${METAL_NAME}_$part\":{\"textures\":\"textures/items/metal/${METAL_NAME}/${METAL_NAME}_$part\"}" >> lang.txt
-done
-echo >> lang.txt
+# echo "// $RP/textures/item_texture.json" >> lang.txt
+# echo ",\"nacht:raw_${METAL_NAME}\":{\"textures\":\"textures/items/metal/${METAL_NAME}/raw_${METAL_NAME}\"}" >> lang.txt
+# for part in ingot sword axe pickaxe shovel hoe boots chestplate helmet leggings; do
+#   echo ",\"nacht:${METAL_NAME}_$part\":{\"textures\":\"textures/items/metal/${METAL_NAME}/${METAL_NAME}_$part\"}" >> lang.txt
+# done
+# echo >> lang.txt
 
-echo "// $RP/textures/textures_list.json" >> lang.txt
-echo ",\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"" >> lang.txt
-echo ",\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"" >> lang.txt
-echo ",\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"" >> lang.txt
-echo ",\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"" >> lang.txt
-echo ",\"textures/items/metal/${METAL_NAME}/raw_${METAL_NAME}\"" >> lang.txt
-echo ",\"textures/items/metal/${METAL_NAME}/${METAL_NAME}_ingot\"" >> lang.txt
-for part in sword axe pickaxe shovel hoe; do
-  echo ",\"textures/items/tools/${METAL_NAME}/${METAL_NAME}_$part\"" >> lang.txt
-done
-for part in boots chestplate helmet leggings; do
-  echo ",\"textures/items/armor/${METAL_NAME}/${METAL_NAME}_$part\"" >> lang.txt
-done
-echo ",\"textures/models/armor/${METAL_NAME}/${METAL_NAME}_main\"" >> lang.txt
-echo ",\"textures/models/armor/${METAL_NAME}/${METAL_NAME}_leggings\"" >> lang.txt
-echo >> lang.txt
+# echo "// $RP/textures/textures_list.json" >> lang.txt
+# echo ",\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"" >> lang.txt
+# echo ",\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"" >> lang.txt
+# echo ",\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"" >> lang.txt
+# echo ",\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"" >> lang.txt
+# echo ",\"textures/items/metal/${METAL_NAME}/raw_${METAL_NAME}\"" >> lang.txt
+# echo ",\"textures/items/metal/${METAL_NAME}/${METAL_NAME}_ingot\"" >> lang.txt
+# for part in sword axe pickaxe shovel hoe; do
+#   echo ",\"textures/items/tools/${METAL_NAME}/${METAL_NAME}_$part\"" >> lang.txt
+# done
+# for part in boots chestplate helmet leggings; do
+#   echo ",\"textures/items/armor/${METAL_NAME}/${METAL_NAME}_$part\"" >> lang.txt
+# done
+# echo ",\"textures/models/armor/${METAL_NAME}/${METAL_NAME}_main\"" >> lang.txt
+# echo ",\"textures/models/armor/${METAL_NAME}/${METAL_NAME}_leggings\"" >> lang.txt
+# echo >> lang.txt
 
-echo "// $RP/textures/terrain_texture.json" >> lang.txt
-echo ",\"${METAL_NAME}_ore\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"}" >> lang.txt
-echo ",\"deepslate_${METAL_NAME}_ore\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"}" >> lang.txt
-echo ",\"raw_${METAL_NAME}_block\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"}" >> lang.txt
-echo ",\"${METAL_NAME}_block\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"}" >> lang.txt
-echo >> lang.txt
+# echo "// $RP/textures/terrain_texture.json" >> lang.txt
+# echo ",\"${METAL_NAME}_ore\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"}" >> lang.txt
+# echo ",\"deepslate_${METAL_NAME}_ore\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"}" >> lang.txt
+# echo ",\"raw_${METAL_NAME}_block\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"}" >> lang.txt
+# echo ",\"${METAL_NAME}_block\":{\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"}" >> lang.txt
+# echo >> lang.txt
 
-echo "// $RP/blocks.json" >> lang.txt
-echo ",\"${METAL_NAME}_ore\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"}" >> lang.txt
-echo ",\"deepslate_${METAL_NAME}_ore\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"}" >> lang.txt
-echo ",\"raw_${METAL_NAME}_block\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"}" >> lang.txt
-echo ",\"${METAL_NAME}_block\":{\"sound\":\"metal\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"}" >> lang.txt
+# echo "// $RP/blocks.json" >> lang.txt
+# echo ",\"${METAL_NAME}_ore\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_ore\"}" >> lang.txt
+# echo ",\"deepslate_${METAL_NAME}_ore\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/deepslate_${METAL_NAME}_ore\"}" >> lang.txt
+# echo ",\"raw_${METAL_NAME}_block\":{\"sound\":\"stone\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/raw_${METAL_NAME}_block\"}" >> lang.txt
+# echo ",\"${METAL_NAME}_block\":{\"sound\":\"metal\",\"textures\":\"textures/blocks/ores/${METAL_NAME}/${METAL_NAME}_block\"}" >> lang.txt
